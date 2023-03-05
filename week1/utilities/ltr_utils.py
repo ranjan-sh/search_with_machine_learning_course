3287a8ba8e6a7715fdbf70dc9348cd8a69c0cb2b
ranjan_shubham <shubham.ranjan@getpostman.com>
2023-03-05 15:46:58 +0530

    implemented 4.e

--- week1/utilities/ltr_utils.py
+++ week1/utilities/ltr_utils.py
@@ -8,9 +8,27 @@ def create_rescore_ltr_query(user_query: str, query_obj, click_prior_query: str,
                              active_features=None, # an array of strings
                              rescore_size=500, main_query_weight=1, rescore_query_weight=2):
     # Create the base query, use a much bigger window
-    #add on the rescore
+    # add on the rescore
     ##### Step 4.e:
-    print("IMPLEMENT ME: create_rescore_ltr_query")
+    # print("IMPLEMENT ME: create_rescore_ltr_query")
+    query_obj["rescore"] = {
+        "window_size": rescore_size,
+        "query": {
+            "rescore_query": {
+                "sltr": {
+                    "params": {
+                        "keywords": user_query
+                    },
+                    "model": ltr_model_name,
+                    # Since we are using a named store, as opposed to simply '_ltr', we need to pass it in
+                    "store": ltr_store_name
+                }
+            },
+            "rescore_query_weight": "2" # Magic number, but let's say LTR matches are 2x baseline matches
+        }
+    }
+
+
     if active_features is not None and len(active_features) > 0:
         query_obj["rescore"]["query"]["rescore_query"]["sltr"]["active_features"] =  active_features
 
