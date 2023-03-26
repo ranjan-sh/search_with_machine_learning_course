3aa702a8767b068a4a45b56e6852d3786fac881e
ranjan_shubham <shubham.ranjan@getpostman.com>
2023-03-26 23:08:04 +0530

    modified query.py

--- utilities/query.py
+++ utilities/query.py
@@ -11,11 +11,13 @@ from urllib.parse import urljoin
 import pandas as pd
 import fileinput
 import logging
-
+import fasttext
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(format='%(levelname)s:%(message)s')
+
+query_model = fasttext.load_model('/workspace/datasets/fasttext/query_model.bin')
 
 # expects clicks and impressions to be in the row
 def create_prior_queries_from_group(
@@ -195,7 +197,15 @@ def search(client, user_query, index="bbuy_products", sort="_score", sortDir="de
     #### W3: classify the query
     #### W3: create filters and boosts
     # Note: you may also want to modify the `create_query` method above
-    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
+    cat_prediction = query_model.predict(user_query, threshold=0.5)
+    filters = []
+    if cat_prediction:
+        categoryid = cat_prediction[0][0].removeprefix('__label__')
+        # print(categoryid)
+        filters.append({ "term": { "categoryPathIds": categoryid   }})
+
+    query_obj = create_query(user_query, click_prior_query=None, filters=filters, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
+
     logging.info(query_obj)
     response = client.search(query_obj, index=index)
     if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
